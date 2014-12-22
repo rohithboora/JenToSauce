6ab5666720ce4757c8354a787b86e0895fffaeff
rohith.boora <rohithboora@gmail.com>
2014-12-22 12:29:30 +0000

    Add some utils to test

---
+++ src/test/java/com/saucelabs/Utils.java
@@ -0,0 +1,16 @@
+package com.saucelabs;
+
+/**
+ * @author Ross Rowe
+ */
+public class Utils {
+
+    public static String readPropertyOrEnv(String key, String defaultValue) {
+        String v = System.getProperty(key);
+        if (v == null)
+            v = System.getenv(key);
+        if (v == null)
+            v = defaultValue;
+        return v;
+    }
+}
--- src/test/java/com/saucelabs/WebDriverTest.java
+++ src/test/java/com/saucelabs/WebDriverTest.java
@@ -1,17 +1,11 @@
 package com.saucelabs;
-
 import com.saucelabs.common.SauceOnDemandAuthentication; 
-import com.saucelabs.common.SauceOnDemandSessionIdProvider; 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestName;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.net.URL;
 
 import static junit.framework.Assert.assertEquals;
@@ -24,18 +18,33 @@ import static junit.framework.Assert.assertEquals;
 public class WebDriverTest {
 
     private WebDriver driver;
-    
+    private String sessionId;
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication();
 
     @Before
 	public void setUp() throws Exception {
 
-    	 DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+    	DesiredCapabilities capabilities = new DesiredCapabilities();
+        String version = Utils.readPropertyOrEnv("SELENIUM_VERSION", "");
+        if (!version.equals("")) {
+            capabilities.setCapability("version", version);
+        }
+        capabilities.setCapability("platform", Utils.readPropertyOrEnv("SELENIUM_PLATFORM", "XP"));
+        capabilities.setCapability("browserName", Utils.readPropertyOrEnv("SELENIUM_BROWSER", "firefox"));
+        String username = Utils.readPropertyOrEnv("SAUCE_USER_NAME", "");
+        String accessKey = Utils.readPropertyOrEnv("SAUCE_API_KEY", "");
+        this.driver = new RemoteWebDriver(
+                new URL("http://" + username + ":" + accessKey + "@ondemand.saucelabs.com:80/wd/hub"),
+                capabilities);
+        //this.sessionId = ((RemoteWebDriver)driver).getSessionId().toString();
+        
+    	 /*DesiredCapabilities capabilities = DesiredCapabilities.firefox();
          capabilities.setCapability("version", "5");
          capabilities.setCapability("platform", Platform.XP);
         
         this.driver = new RemoteWebDriver(
 					  new URL("http://rohithboora:f55a6595-3f95-4da3-a0f2-c330059dcd50@ondemand.saucelabs.com:80/wd/hub"),
-					  capabilities);
+					  capabilities);*/
     }
 
     @Test
